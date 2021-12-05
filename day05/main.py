f61f8e2858976eb123a54ae1cd682af07e5f6038
Wouter van Heijst <wouter.vanheijst@wartsila.com>
2021-12-05 11:11:47 +0200

    WIP

--- day05/main.py
+++ day05/main.py
@@ -17,17 +17,31 @@ segments = pd.DataFrame(data=segments, columns=["x0", "y0", "x1", "y1"])
 xmax = segments[['x1', 'x0']].max().max()
 ymax = segments[['y1', 'y0']].max().max()
 
-field = pd.DataFrame(data=np.zeros(shape=(xmax + 1, ymax + 1)))
-
 segments['slope'] = (segments['y1'] - segments['y0']) / (segments['x1'] - segments['x0'])
-# assert all integer
+print(segments['slope'].value_counts())
 
-for ix, (x0, y0, x1, y1, slope) in segments.iterrows():
-    x0, y0, x1, y1 = map(int, [x0, y0, x1, y1])
-    if abs(slope) == 0 or not np.isfinite(slope):
-        # Either x or y is constant, we can just sort both to get increasing coordinates
-        a, b = sorted([x0, x1])
-        c, d = sorted([y0, y1])
-        field.loc[a:b, c:d] = field.loc[a:b, c:d] + 1
+def heightmap(diagonal):
+    field = pd.DataFrame(data=np.zeros(shape=(xmax + 1, ymax + 1)))
+    for ix, (x0, y0, x1, y1, slope) in segments.iterrows():
+        x0, y0, x1, y1 = map(int, [x0, y0, x1, y1])
+        if abs(slope) == 0 or not np.isfinite(slope):
+            # Either x or y is constant, we can just sort both to get increasing coordinates
+            a, b = sorted([x0, x1])
+            c, d = sorted([y0, y1])
+            field.loc[a:b, c:d] = field.loc[a:b, c:d] + 1
+        elif diagonal:
+            islope = int(slope)
+            start, end = sorted([[x0, y0], [x1, y1]])
+            cur = start
+            field.loc[cur[0], cur[1]] = field.loc[cur[0], cur[1]] + 1
+            while cur != end:
+                cur[0] = cur[0] + 1
+                cur[1] = cur[1] + islope
+                field.loc[cur[0], cur[1]] = field.loc[cur[0], cur[1]] + 1
+    print(field)
+    print(len(field.stack()[field.stack() > 1]))
+    return field
 
-print(len(field.stack()[field.stack() > 1]))
+
+heightmap(False)
+heightmap(True)
