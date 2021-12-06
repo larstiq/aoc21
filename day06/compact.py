f779793990893a916a8e4076c7a178ce1f09a7bc
Wouter van Heijst <wouter.vanheijst@wartsila.com>
2021-12-06 14:59:22 +0200

    WIP

---
+++ day06/compact.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import numpy as np
+
+with open("input") as puzzle_input:
+    fishes = pd.read_csv(puzzle_input, header=None).transpose()
+
+fish_generations = pd.Series(
+    fishes.value_counts().get(generation, 0) for generation in range(9)
+)
+
+T = np.roll(np.identity(9, dtype=int), -1, axis=0)
+T[6, 0] = 1
+P = np.linalg.matrix_power(T, 256)
+print(P.dot(fish_generations).sum().sum())
