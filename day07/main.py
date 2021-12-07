4a04ef3be89806267ec24a45c1e852f99ff4060b
Wouter van Heijst <wouter.vanheijst@wartsila.com>
2021-12-07 09:46:56 +0200

    WIP

--- day07/main.py
+++ day07/main.py
@@ -7,10 +7,8 @@ import time
 with open("input") as puzzle_input:
     crabs = pd.read_csv(puzzle_input, header=None).loc[0]
 
-# Can't be worse
 possible_positions = np.linspace(crabs.min(), crabs.max(), crabs.max() - crabs.min() + 1)
 
-# TODO: crab gens
 vc = crabs.value_counts(sort=False)
 crab_positions = pd.Series(data=vc.index, index=vc.values)
 
@@ -33,5 +31,7 @@ def part2():
     all_costs = fuel_costs.T.dot(vc)
     print(all_costs.min(), all_costs.argmin())
 
+
+# TODO: integer programming optimization?
 
 part2()
