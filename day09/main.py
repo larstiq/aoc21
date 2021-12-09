005bbd8f8b7e66ad042bd808679bb5d95203c684
Wouter van Heijst <wouter.vanheijst@wartsila.com>
2021-12-09 12:22:44 +0200

    WIP

--- day09/main.py
+++ day09/main.py
@@ -3,8 +3,7 @@
 import pandas as pd
 import numpy as np
 import time
-
-with open("input") as puzzle_input:
+with open("simput") as puzzle_input:
     data = [list(map(int, line.strip())) for line in puzzle_input]
     print(data)
     # TODO: assert wires[10] == '1'
@@ -22,3 +21,84 @@ with open("input") as puzzle_input:
     print(low_risk.sum().sum())
 
 
+    semi_low_mask = ((df <= df.shift(1).fillna(fill_val)) & (df <= df.shift(-1).fillna(fill_val)) &
+                     (df <= df.shift(1, axis=1).fillna(fill_val)) & (df <= df.shift(-1, axis=1).fillna(fill_val)))
+
+
+    print(df[low_mask])
+
+    basins = df[semi_low_mask]
+
+    semis = [
+        (df <= df.shift(1).fillna(fill_val)),
+        (df <= df.shift(-1).fillna(fill_val)),
+        (df <= df.shift(1, axis=1).fillna(fill_val)),
+        (df <= df.shift(-1, axis=1).fillna(fill_val))
+    ]
+
+    #for ix, semi in enumerate(semis):
+        #    print(ix, '-' * 80)
+        #print(df[semi])
+
+    oned = low_mask.stack()
+    oned[oned == False] = np.nan
+    basin_locations = oned.dropna().index
+    
+    basins = [pd.DataFrame(index=df.index, columns=df.columns, data=False) for ix in basin_locations]
+    for ix, basin in enumerate(basin_locations):
+        print("Basin", ix)
+        basins[ix].loc[basin] = True
+        print(basins[ix])
+
+    #print(basins)
+
+    #for ix in range(3):
+        #    semi_low_mask = ((df <= df.shift(1).fillna(fill_val)) | (df <= df.shift(-1).fillna(fill_val)) |
+        #                         (df <= df.shift(1, axis=1).fillna(fill_val)) | (df <= df.shift(-1, axis=1).fillna(fill_val)))
+        #print(df[semi_low_mask])
+        
+    #for flood_level in range(1, 9):
+        #     print(df[df < flood_level])
+
+
+    roundf = 0
+    while True:
+
+        stagnants = [False] #* len(basins)
+        roundf = roundf + 1
+        print("Round", roundf)
+        for ix, basin in enumerate(basins):
+            if ix == 0:
+                breakpoint()
+            obasin = basin.copy()
+
+            growths = []
+            for semi in [(0, 1), (0, -1), (1, 1), (1, -1)]:
+                #breakpoint()
+
+                sh = obasin.shift(semi[1], axis=semi[0])
+                growths.append(df[sh] < 9)
+
+            breakpoint() 
+
+            stagnants[ix] = any(g.any().any() for g in growths)
+            grow = growths[0]
+            for g in growths:
+                grow = grow | g
+
+            if ix == 0:
+                print(grow)
+                breakpoint()
+            basins[ix] = basins[ix] | grow
+        if all(stagnants):
+            break
+
+    
+    # sizes of basins
+    cum = 1
+    for b in basins:
+        size = b.sum().sum()
+        cum = cum * size
+        print(size)
+        print(df[b])
+    print(cum)
