f5e87480dc49d6760780bb29852c3666e15c7e9e
Wouter van Heijst <wouter.vanheijst@wartsila.com>
2021-12-11 10:47:56 +0200

    cleanup

--- day11/main.py
+++ day11/main.py
@@ -28,49 +28,34 @@ with open("input") as puzzle_input:
     data = [list(map(int, line.strip())) for line in puzzle_input]
     octopi = pd.DataFrame(data=data)
 
-    debug = False
     flashes = 0
-    dancing = False
-    step = 1
-    while not dancing:
+    synchronized = False
+    step = 0
+    while not synchronized:
+        step = step + 1
         flashed = np.zeros_like(octopi, dtype=bool)
         octopi = octopi + 1
         charged = octopi > 9
-        show_octopi("after charge", octopi)
-        show_octopi("about to flash", charged)
 
-        #breakpoint()
         while charged.any().any():
             df = charged.stack()
             for location in df[df].index:
                 for direction in directions:
                     neighbour = location[0] + direction[0], location[1] + direction[1]
 
-                    #if neighbour == (0, 1) or location == (0, 2) or location == (1, 1):
-                        #    pass
-                        
-                    #if neighbour == (0, 1):
-                        #    breakpoint()
-
                     if (neighbour[0] >= 0 and neighbour[1] >= 0 and
                         neighbour[0] < octopi.shape[0] and
                         neighbour[1] < octopi.shape[1]):
                         octopi.loc[neighbour] = octopi.loc[neighbour] + 1
-                    #flash_neighbours = pd.DataFrame(morphology.grey_dilation(new_nines, footprint=flash_struc, mode='constant', cval=-1))
-                    #print("flashn", flash_neighbours)
-                    #breakpoint()
-                    #breakpoint()
+
             flashed = flashed | charged
             charged = (octopi > 9) & ~flashed
-            if debug:
-                show_octopi("new nines", charged)
-                show_octopi("flashed", flashed)
-                show_octopi("octopi", octopi)
 
         octopi[flashed] = 0
+
         flashes = flashes + flashed.sum().sum()
-        print("Flashes", flashes)
-        show_octopi(f"End of step {step}", octopi)
-        print('-' * 80)
-        step = step + 1
-        dancing = flashed.all().all()
+        if step == 100:
+            print("Flashes", flashes)
+        synchronized = flashed.all().all()
+        if synchronized:
+            print("Synchronized at step", step)
