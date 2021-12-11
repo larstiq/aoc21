19dcf42da15160a89973eaa66d814498557c7d61
Wouter van Heijst <wouter.vanheijst@wartsila.com>
2021-12-11 09:46:48 +0200

    WIP

--- day11/main.py
+++ day11/main.py
@@ -10,6 +10,12 @@ flash_struc = np.array([
         [1, 0, 1],
         [1, 1, 1]], dtype=bool)
 
+directions = [
+    (-1, -1), (-1, 0), (-1, 1),
+    ( 0, -1),          ( 0, 1),
+    ( 1, -1), ( 1, 0), ( 1, 1),
+]
+
 with open("tinput") as puzzle_input:
     data = [list(map(int, line.strip())) for line in puzzle_input]
     octopi = pd.DataFrame(data=data)
@@ -17,24 +23,30 @@ with open("tinput") as puzzle_input:
     flashes = 0
     for step in range(3):
         flashed = np.zeros_like(octopi, dtype=bool)
-        charge = octopi + 1
-        nines = charge > 9
+        octopi = octopi + 1
+        nines = octopi > 9
         flashed = nines
         new_nines = nines
-        print(charge)
+        print(octopi)
         print(nines)
 
-        print(flash_neighbours)
-
         while new_nines.any().any():
-            flash_neighbours = pd.DataFrame(morphology.grey_dilation(new_nines, footprint=flash_struc, mode='constant', cval=-1))
-            print("flashn", flash_neighbours)
-            charge[flash_neighbours] = charge[flash_neighbours] + 1
-            print(charge)
-            new_nines = (charge > 9) & ~flashed
-            print(new_nines)
-            flashed = flashed | flash_neighbours
-            print(flashed)
+            for location in new_nines.stack()[new_nines.stack()].index:
+                for direction in directions:
+                    neighbour = location[0] + direction[0], location[1] + direction[1]
 
-        charge[flashed] = 0
-        octopi = charge
+                    #breakpoint()
+                    octopi.loc[neighbour] = octopi.loc[neighbour] + 1
+                    #flash_neighbours = pd.DataFrame(morphology.grey_dilation(new_nines, footprint=flash_struc, mode='constant', cval=-1))
+                    #print("flashn", flash_neighbours)
+                    #breakpoint()
+            flashed = flashed | new_nines
+            new_nines = (octopi > 9) & ~flashed
+            print(new_nines)
+            print(flashed)
+            print(octopi)
+
+        octopi[flashed] = 0
+        print("End of step", step)
+        print(octopi)
+        print('-' * 80)
