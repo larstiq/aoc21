99bf1ae6193396da2ed90d3f236a45a2af822e49
Wouter van Heijst <wouter.vanheijst@wartsila.com>
2021-12-12 11:05:51 +0200

    WIP

--- day12/main.py
+++ day12/main.py
@@ -7,31 +7,10 @@ from scipy.ndimage.measurements import label, morphology
 import networkx as nx
 import matplotlib
 
-with open("simput") as puzzle_input:
-    #data = pd.read_csv(puzzle_input, sep=0, header=None)
-    data = [line.strip().split("-") for line in puzzle_input]
-
+def to_graph(text):
+    data = [line.strip().split("-") for line in text]
     G = nx.from_edgelist(data, create_using=nx.MultiGraph)
-
-    #for node in G.nodes:
-    #    if node.upper() == node:
-    #        G.add_edge(node, node)
-
-    simple_paths = list(nx.all_simple_paths(G, source='start', target='end'))
-
-
-nx.draw(G)
-nx.draw(nx.bfs_tree(G, 'start'))
-visited_small = set()
-current = 'start'
-paths = {}
-
-print(G[current])
-
-for neighbour in G[current]:
-    if neighbour.lower() == neighbour and neighbour not in visited_small:
-        path_so_far.extend(neighbour)
-        
+    return G
 
 def rec_visit(G, path, visited):
     current = path[-1]
@@ -44,38 +23,93 @@ def rec_visit(G, path, visited):
         elif neighbour.upper() == neighbour and current != neighbour:
             # TODO: cycles?
             subpaths = rec_visit(G, path + [neighbour], visited)
+        else:
+            subpaths = []
 
-        paths.extend(subpath)
+        paths.extend(subpaths)
 
     return paths
 
 
-    
+smallout = """start,A,b,A,c,A,end
+start,A,b,A,end
+start,A,b,end
+start,A,c,A,b,A,end
+start,A,c,A,b,end
+start,A,c,A,end
+start,A,end
+start,b,A,c,A,end
+start,b,A,end
+start,b,end"""
+
+smallout_paths = [line.strip().split(",") for line in testtxt.split("\n")] 
 
 
+midin = """dc-end
+HN-start
+start-kj
+dc-start
+dc-HN
+LN-dc
+HN-end
+kj-sa
+kj-HN
+kj-dc"""
 
-list(nx.all_simple_paths(G, source='A', target='A'))
 
-len(simple_paths)
+mid_graph = to_graph(midin.split("\n"))
+midout  = """start,HN,dc,HN,end
+start,HN,dc,HN,kj,HN,end
+start,HN,dc,end
+start,HN,dc,kj,HN,end
+start,HN,end
+start,HN,kj,HN,dc,HN,end
+start,HN,kj,HN,dc,end
+start,HN,kj,HN,end
+start,HN,kj,dc,HN,end
+start,HN,kj,dc,end
+start,dc,HN,end
+start,dc,HN,kj,HN,end
+start,dc,end
+start,dc,kj,HN,end
+start,kj,HN,dc,HN,end
+start,kj,HN,dc,end
+start,kj,HN,end
+start,kj,dc,HN,end
+start,kj,dc,end"""
 
-H = G.copy()
-for node in G:
-    if node.upper() == node:
-        for neighbour in G[node]:
-            if neighbour.lower() == neighbour:
-                for other in G[node]:
-                    if other != neighbour and other.lower() == other:
-                        replace_node = f"{neighbour}-replace-{node}-{other}"
-                        H.add_node(replace_node)
-                        H.add_edge(neighbour, replace_node)
-                        H.add_edge(replace_node, other)
+mid_paths = [line.strip().split(",") for line in midout.split("\n")]
 
-nx.draw_networkx(H)
+assert set(tuple(l) for l in rec_visit(mid_graph, ['start'], {'start'})) == set(tuple(l) for l in mid_paths)
 
-nx.draw_networkx(nx.path_graph(G))
 
-help(nx.path_graph)
+bigin = """fs-end
+he-DX
+fs-he
+start-DX
+pj-DX
+end-zg
+zg-sl
+zg-pj
+pj-he
+RW-he
+fs-DX
+pj-RW
+zg-RW
+start-pj
+he-WI
+zg-he
+pj-fs
+start-RW"""
+big_graph = to_graph(bigin.split("\n"))
+big_result = rec_visit(big_graph, ['start'], {'start'})
 
-list(nx.all_simple_paths(H, source='start', target='end'))
 
-simple_paths
+with open("simput") as puzzle_input:
+    #data = pd.read_csv(puzzle_input, sep=0, header=None)
+    G = to_graph([line for line in puzzle_input])
+    aa = rec_visit(G, ['start'], {'start'})
+    print(aa)
+    assert set(tuple(l) for l in aa) == set(tuple(l) for l in smallout_paths)
+
+
