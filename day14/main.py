9a330ad1b2eb7aec16208235deef79fefb3ff6d5
Wouter van Heijst <wouter.vanheijst@wartsila.com>
2021-12-14 12:17:51 +0200

    Duh

--- day14/main.py
+++ day14/main.py
@@ -1,10 +1,7 @@
 #!/usr/bin/env python3.10
 
-#import pandas as pd
-#import numpy as np
 import itertools
 from collections import Counter
-
 
 pairs = {}
 with open("input") as puzzle_input:
@@ -14,42 +11,24 @@ with open("input") as puzzle_input:
         left, right = line.strip().split(" -> ")
         pairs[tuple(left)] = right
 
-    print(pairs)
-    for step in range(10):
-        new_polymer = []
-        for ix, char in enumerate(polymer):
-            new_polymer.append(char)
-            try:
-                new_polymer.append(pairs[(char, polymer[ix + 1])])
-            except:
-                pass
-
-        polymer = new_polymer
-        print(len(polymer))
-
-    count = Counter(polymer)
-    print(count)
-    print(count.most_common()[0][1] - count.most_common()[-1][1])
-
-    
-
-def with_counter():
+    char_count = Counter(polymer)
     polymer_count = Counter()
 
     for pair in itertools.pairwise(polymer):
         polymer_count[pair] += 1
 
-    print(polymer_count)
-
-    for step in range(10):
+    for step in range(40):
         new_polymer = Counter()
         for pair in polymer_count:
-            new_polymer[(pair[0], pairs[pair])] += 1
-            new_polymer[(pairs[pair], pair[1])] += 1
+
+
+            count = polymer_count[pair]
+            insert_char = pairs[pair]
+            new_polymer[(pair[0], insert_char)] += count
+            new_polymer[(insert_char, pair[1])] += count
+            char_count[insert_char] += count
 
         polymer_count = new_polymer
 
     print(polymer_count)
-
-    
-
+    print(char_count.most_common()[0][1] - char_count.most_common()[-1][1])
