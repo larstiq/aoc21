f8658220de42efd9ceb370c970a8f555fc36ace2
Wouter van Heijst <wouter.vanheijst@wartsila.com>
2021-12-16 09:50:50 +0200

    WIP

--- day16/main.py
+++ day16/main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 
-def hex2bin(c):
+def hex2bin(hexchar):
     mapping = {
         "0": "0000",
         "1": "0001",
@@ -20,37 +20,75 @@ def hex2bin(c):
         "E": "1110",
         "F": "1111",
     }
-    return mapping[c]
+    return mapping[hexchar]
+
+
+def parse_message(bits, total_length=None, number_of_packets=None):
+    if bits == '':
+        breakpoint()
+    index = 6
+
+
+    exhausted = False
+    while not exhausted:
+        version = int(bits[:3], 2)
+        typeID = bits[3:6]
+
+        if typeID == "100":
+            more = True
+            literal = ""
+            while more:
+                nibble = bits[index:index + 5]
+                if nibble[0] == '0':
+                    more = False
+                index += len(nibble)
+                literal += nibble[1:]
+                print(int(literal, 2), nibble, more)
+            packet = (version, typeID, literal)
+            subpackets = []
+        else:
+            # operator
+            length_typeID = bits[index:index + 1]
+            index += 1
+
+            if length_typeID == "0":
+                arg = int(bits[index:index + 15], 2)
+                index += 15
+                packet = (version, "operator", typeID, "length", arg)
+                breakpoint()
+                subpackets = parse_message(bits[index:], total_length=arg)
+            elif length_typeID == "1":
+                arg = int(bits[index:index + 11], 2)
+                index += 11
+                packet = (version, "operator", typeID, "number", arg)
+                breakpoint()
+                subpackets = parse_message(bits[index:], number_of_packets=arg)
+            else:
+                breakpoint()
+
+        if (index >= len(bits) or
+           (total_length is not None and total_length < index) or
+           (number_of_packets is not None and number_of_packets < len(subpackets))):
+            exhausted = True
+
+
+
+    return [packet] + subpackets
+
 
 
 with open("input") as puzzle_input:
 
     message = puzzle_input.read().strip()
     message = "D2FE28"
+    message = "38006F45291200"
 
     bits_list = []
-    for c in message:
-        bits_list.extend(hex2bin(c))
+    for hexchar in message:
+        bits_list.extend(hex2bin(hexchar))
 
     bits = "".join(bits_list)
 
+
+    result = parse_message(bits)
     
-    version = bits[:3]
-    typeID = bits[3:6]
-    index = 6
-
-    if typeID == "100":
-        more = True
-        literal = ""
-        while more:
-            nibble = bits[index:index + 5]
-            if nibble[0] == '0':
-                more = False
-            index += 5
-            literal += nibble[1:]
-            print(literal, nibble, more)
-
-
-    print(message)
-
-
