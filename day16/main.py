bbcf31fa2a7236817266348d873cf0ff5a717177
Wouter van Heijst <wouter.vanheijst@wartsila.com>
2021-12-16 21:21:59 +0200

    WIP

--- day16/main.py
+++ day16/main.py
@@ -25,20 +25,22 @@ def hex2bin(hexchar):
     return mapping[hexchar]
 
 
-def parse_message(bits, tree, parent=None, number_of_packets=None):
-    print("Parsing message of length", len(bits))
-    index = 0
+def parse_message(bits, index, tree, parent=None, number_of_packets=None):
+    #print("Parsing message of length", len(bits))
 
 
     exhausted = False
     parsed_packets = 0
     while not exhausted:
+        packet_start = index
         version = int(bits[index:index + 3], 2)
         typeID = bits[index + 3:index + 6]
         index += 6
 
         if typeID == "100":
             print("literal starting at", index)
+            #if index == 108:
+                #    breakpoint()
             more = True
             literal = ""
             while more:
@@ -51,16 +53,15 @@ def parse_message(bits, tree, parent=None, number_of_packets=None):
                     more = False
                 index += len(nibble)
                 literal += nibble[1:]
-                print(int(literal, 2), nibble, more)
-                print(bits)
-                print(bits[:index])
-            packet = (version, typeID, "literal", int(literal, 2))
-
+                #print(int(literal, 2), nibble, more)
+                #print(bits)
+                #print(bits[:index])
+            packet = (version, packet_start, typeID, "literal", int(literal, 2))
             tree.add_node(packet)
             if parent:
                 tree.add_edge(parent, packet)
             else:
-                return
+                return index
         else:
             # operator
             length_typeID = bits[index:index + 1]
@@ -69,31 +70,35 @@ def parse_message(bits, tree, parent=None, number_of_packets=None):
             if length_typeID == "0":
                 total_length = int(bits[index:index + 15], 2)
                 index += 15
-                packet = (version, "operator", typeID, "length", total_length)
+                packet = (version, packet_start, "operator", typeID, "length", total_length)
 
                 tree.add_node(packet)
 
                 #breakpoint()
-                parse_message(bits[index:index + total_length], tree, packet)
+                advanced = parse_message(bits, index, tree, packet)
+                index += total_length
+
                 if parent:
                     tree.add_edge(parent, packet)
                 else:
-                    return
+                    return index
             elif length_typeID == "1":
                 arg = int(bits[index:index + 11], 2)
                 index += 11
-                packet = (version, "operator", typeID, "number", arg)
+                packet = (version, packet_start, "operator", typeID, "number", arg)
 
                 tree.add_node(packet)
                 #breakpoint()
-                parse_message(bits[index:], tree, packet, number_of_packets=arg)
+                advanced = parse_message(bits, index, tree, packet, number_of_packets=arg)
 
                 if parent:
                     tree.add_edge(parent, packet)
                 else:
-                    return
+                    return advanced
             else:
-                breakpoint()
+                # Bits have run out, why?
+                #breakpoint()
+                return index
 
             parsed_packets += 1
 
@@ -101,12 +106,13 @@ def parse_message(bits, tree, parent=None, number_of_packets=None):
            (number_of_packets is not None and number_of_packets < parsed_packets)):
             exhausted = True
 
+        return index
+
 
 
 
 with open("input") as puzzle_input:
 
-    message = puzzle_input.read().strip()
     message = "D2FE28"
     message = "38006F45291200"
     message = "EE00D40C823060"
@@ -114,6 +120,7 @@ with open("input") as puzzle_input:
     message = "620080001611562C8802118E34"
     message = "C0015000016115A2E0802F182340"
     message = "A0016C880162017C3686B18A3D4780"
+    message = puzzle_input.read().strip()
 
     bits_list = []
     for hexchar in message:
@@ -123,6 +130,6 @@ with open("input") as puzzle_input:
 
 
     tree = nx.DiGraph()
-    result = parse_message(bits, tree)
+    result = parse_message(bits, 0, tree)
     print(tree.nodes, sum(n[0] for n in tree.nodes))
     
