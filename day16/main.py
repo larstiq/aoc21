3c5b722e6c75328c9766373454d09a6635bb77fa
Wouter van Heijst <wouter.vanheijst@wartsila.com>
2021-12-19 22:14:43 +0200

    WIP

--- day16/main.py
+++ day16/main.py
@@ -2,6 +2,7 @@
 
 import networkx as nx
 import operator
+from functools import reduce
 
 
 def hex2bin(hexchar):
@@ -25,86 +26,98 @@ def hex2bin(hexchar):
     }
     return mapping[hexchar]
 
-
-typeID_map = {
-    "000": sum,
-    "001": operator.mul,
+typeID_map = { "000": sum, "001": lambda args: reduce(operator.mul, args, 1),
     "010": min,
     "011": max,
-    "100": literal,
-    "101": operator.gt, 
-    "110": operator.lt,
-    "111": operator.eq,
+    "100": lambda args: args,
+    "101": lambda args: 1 * (args[0] > args[1]),
+    "110": lambda args: 1 * (args[0] < args[1]),
+    "111": lambda args: 1 * (args[0] == args[1]),
 }
 
 
-def dummy(*x):
-    pass
-
-def parse_message(bits, index, tree, parent=None, number_of_packets=None, max_index=None):
-    #print("Parsing message of length", len(bits))
-
+def parse_message(bits, index, tree, number_of_packets=None, max_index=None):
+    # print("Parsing message of length", len(bits))
 
     exhausted = False
-    parsed_packets = 0
+    parsed_packets = []
     while not exhausted:
+        subpackets = []
         packet_start = index
-        version = int(bits[index:index + 3], 2)
-
-        typeID = bits[index + 3:index + 6]
+        version = int(bits[index : index + 3], 2)
+        typeID = bits[index + 3 : index + 6]
+        func = typeID_map[typeID]
         index += 6
 
         if typeID == "100":
             more = True
             literal = ""
             while more:
-                nibble = bits[index:index + 5]
-                if nibble[0] == '0':
+                nibble = bits[index : index + 5]
+                if nibble[0] == "0":
                     more = False
                 index += len(nibble)
                 literal += nibble[1:]
-                packet = (version, packet_start, typeID, dummy, literal, int(literal, 2))
+            val = int(literal, 2)
+            packet = (version, packet_start, typeID, val, literal)
             tree.add_node(packet)
         else:
             # operator
-            length_typeID = bits[index:index + 1]
+            length_typeID = bits[index : index + 1]
             index += 1
 
             if length_typeID == "0":
-                total_length = int(bits[index:index + 15], 2)
+                total_length = int(bits[index : index + 15], 2)
                 index += 15
-                packet = (version, packet_start, typeID,  typeID_map[typeID], "length", total_length)
 
-                tree.add_node(packet)
-
-                advanced, _ = parse_message(bits[:index + total_length], index, tree, packet, max_index=index + total_length)
+                advanced, subpackets = parse_message(
+                    bits[: index + total_length],
+                    index,
+                    tree,
+                    max_index=index + total_length,
+                )
                 index += total_length
                 assert index == advanced
-            elif length_typeID == "1":
-                arg = int(bits[index:index + 11], 2)
-                index += 11
-                packet = (version, packet_start, typeID,  typeID_map[typeID], "number", arg)
 
+                val = func([p[3] for p in subpackets])
+                packet = (version, packet_start, typeID, val, "length", total_length)
                 tree.add_node(packet)
-                advanced,_ = parse_message(bits, index, tree, packet, number_of_packets=arg)
+            elif length_typeID == "1":
+                arg = int(bits[index : index + 11], 2)
+                index += 11
+
+                advanced, subpackets = parse_message(
+                    bits,
+                    index,
+                    tree,
+                    number_of_packets=arg,
+                )
+
+                val = func([p[3] for p in subpackets])
+                packet = (version, packet_start, typeID, val, "number", arg)
+                tree.add_node(packet)
                 index = advanced
             else:
                 breakpoint()
-                return index, parsed_packets
+                1 / 0
 
+        for child in subpackets:
+            tree.add_edge(packet, child)
+        parsed_packets.append(packet)
 
-        parsed_packets += 1
-        if parent:
-            tree.add_edge(parent, packet)
-
-        if (index >= len(bits) or
-            (number_of_packets is not None and number_of_packets >= parsed_packets) or
-            (max_index is not None and index >= max_index)
-           ):
+        if (
+            index >= len(bits)
+            or (
+                number_of_packets is not None
+                and number_of_packets <= len(parsed_packets)
+            )
+            or (max_index is not None and index >= max_index)
+        ):
             exhausted = True
 
+    #if number_of_packets is not None and number_of_packets != len(subpacek
+    #    breakpoint()
     return index, parsed_packets
-
 
 
 def go(message):
@@ -114,77 +127,70 @@ def go(message):
 
     bits = "".join(bits_list)
 
-
     tree = nx.DiGraph()
     result = parse_message(bits, 0, tree, number_of_packets=1)
 
-    root = list(tree.nodes)[0]
-    def get_v(node):
-
-        f = node[3]
-        print(f, tree[node])
-        gar = []
-        for c in tree[node]:
-            cr = get_v(c)
-            gar.append(cr)
-
-        print(gar)
-        if f is dummy:
-            return node[-1]
-        return f(*gar)
-
-
-    print(get_v(root))
-
-
     version_sum = sum(n[0] for n in tree.nodes)
+    root = list(nx.topological_sort(tree))[0]
 
-    return list(tree.nodes), version_sum
-
-
-with open("input") as puzzle_input:
-
-    go("D2FE28")
-    go("04005AC33890")
+    return list(tree.nodes), version_sum, root[3]
 
 
-    if False:
-        assert [(6, 0, '100', 'literal', 2021)], 6 == go("D2FE28")
-        assert [(1, 0, 'operator', '110', 'length', 27), (6, 22, '100', 'literal', 10), (2, 33, '100', 'literal', 20)], 9 == go("38006F45291200")
-        assert [(7, 0, 'operator', '011', 'number', 3),
-         (2, 18, '100', 'literal', 1),
-         (4, 29, '100', 'literal', 2),
-         (1, 40, '100', 'literal', 3)], 14 == go("EE00D40C823060")
+def main():
+    with open("input") as puzzle_input:
+        assert [(6, 0, "100", "literal", 2021)], 6 == go("D2FE28")
+        assert [
+            (1, 0, "operator", "110", "length", 27),
+            (6, 22, "100", "literal", 10),
+            (2, 33, "100", "literal", 20),
+        ], 9 == go("38006F45291200")
+        assert [
+            (7, 0, "operator", "011", "number", 3),
+            (2, 18, "100", "literal", 1),
+            (4, 29, "100", "literal", 2),
+            (1, 40, "100", "literal", 3),
+        ], 14 == go("EE00D40C823060")
 
-        assert [(4, 0, 'operator', '010', 'number', 1),
-                (1, 18, 'operator', '010', 'number', 1),
-                (5, 36, 'operator', '010', 'length', 11),
-                (6, 58, '100', 'literal', 15)], 16 == go("8A004A801A8002F478")
-        assert [(3, 0, 'operator', '000', 'number', 2),
-                (0, 18, 'operator', '000', 'length', 22),
-                (0, 40, '100', 'literal', 10),
-                (5, 51, '100', 'literal', 11),
-                (1, 62, 'operator', '000', 'number', 2),
-                (0, 80, '100', 'literal', 12),
-                (3, 91, '100', 'literal', 13)], 12 == go("620080001611562C8802118E34")
+        assert [
+            (4, 0, "operator", "010", "number", 1),
+            (1, 18, "operator", "010", "number", 1),
+            (5, 36, "operator", "010", "length", 11),
+            (6, 58, "100", "literal", 15),
+        ], 16 == go("8A004A801A8002F478")
+        assert [
+            (3, 0, "operator", "000", "number", 2),
+            (0, 18, "operator", "000", "length", 22),
+            (0, 40, "100", "literal", 10),
+            (5, 51, "100", "literal", 11),
+            (1, 62, "operator", "000", "number", 2),
+            (0, 80, "100", "literal", 12),
+            (3, 91, "100", "literal", 13),
+        ], 12 == go("620080001611562C8802118E34")
 
-        assert [(6, 0, 'operator', '000', 'length', 84),
-                (0, 22, 'operator', '000', 'length', 22),
-                (0, 44, '100', 'literal', 10),
-                (6, 55, '100', 'literal', 11),
-                (4, 66, 'operator', '000', 'number', 2),
-                (7, 84, '100', 'literal', 12),
-                (0, 95, '100', 'literal', 13)], 23 == go("C0015000016115A2E0802F182340")
+        assert [
+            (6, 0, "operator", "000", "length", 84),
+            (0, 22, "operator", "000", "length", 22),
+            (0, 44, "100", "literal", 10),
+            (6, 55, "100", "literal", 11),
+            (4, 66, "operator", "000", "number", 2),
+            (7, 84, "100", "literal", 12),
+            (0, 95, "100", "literal", 13),
+        ], 23 == go("C0015000016115A2E0802F182340")
 
-        assert [(5, 0, 'operator', '000', 'length', 91),
-                (1, 22, 'operator', '000', 'number', 1),
-                (3, 40, 'operator', '000', 'number', 5),
-                (7, 58, '100', 'literal', 6),
-                (6, 69, '100', 'literal', 6),
-                (5, 80, '100', 'literal', 12),
-                (2, 91, '100', 'literal', 15),
-                (2, 102, '100', 'literal', 15)], 31 == go("A0016C880162017C3686B18A3D4780")
+        assert [
+            (5, 0, "operator", "000", "length", 91),
+            (1, 22, "operator", "000", "number", 1),
+            (3, 40, "operator", "000", "number", 5),
+            (7, 58, "100", "literal", 6),
+            (6, 69, "100", "literal", 6),
+            (5, 80, "100", "literal", 12),
+            (2, 91, "100", "literal", 15),
+            (2, 102, "100", "literal", 15),
+        ], 31 == go("A0016C880162017C3686B18A3D4780")
 
 
+        breakpoint()
         assert 917 == go(puzzle_input.read().strip())[1]
-    
+
+
+main()
